ce6d527055c4a9f123426dd9a76a25d78dbf804e
Simon <lx.simon@yahoo.com>
2017-09-26 17:15:25 +0800

    add chinese analyzer

--- tokenizer/src/main/java/cn/sfck/tokenizer/Stanford.java
+++ tokenizer/src/main/java/cn/sfck/tokenizer/Stanford.java
@@ -11,6 +11,11 @@ import java.util.Properties;
 
 public class Stanford {
     public static void main(String[] args) {
+        Stanford stanford = new Stanford();
+        stanford.tokenizerChinese();
+        stanford.tokenizerEnglist();
+    }
+    public void tokenizerEnglist() {
         Properties props = new Properties();
 //        props.setProperty("annotators", "tokenize,ssplit,pos,lemma,parse,natlog");
         props.setProperty("annotators", "tokenize, ssplit");
@@ -25,8 +30,30 @@ public class Stanford {
                 String word = token.get(CoreAnnotations.TextAnnotation.class);
                 System.out.println(word);
             }
+            System.out.println("======================");
         }
-
+    }
+    public void tokenizerChinese() {
+        Properties pros = new Properties();
+        pros.setProperty("annotators", "tokenize, ssplit");
+        pros.setProperty("tokenize.language", "zh");
+        pros.setProperty("segment.model", "edu/stanford/nlp/models/segmenter/chinese/ctb.gz");
+        pros.setProperty("segment.sighanCorporaDict", "edu/stanford/nlp/models/segmenter/chinese");
+        pros.setProperty("segment.serDictionary", "edu/stanford/nlp/models/segmenter/chinese/dict-chris6.ser.gz");
+        pros.setProperty("segment.sighanPostProcessing", "true");
+        pros.setProperty("ssplit.boundaryTokenRegex", "[.。]|[!?！？]+");
+        StanfordCoreNLP corenlp = new StanfordCoreNLP(pros);
+        String text = "克林顿说，华盛顿将逐步落实对韩国的经济援助。金大中对克林顿的讲话报以掌声：克林顿总统在会谈中重申，他坚定地支持韩国摆脱经济危机。";
+        Annotation document = new Annotation(text);
+        corenlp.annotate(document);
+        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+        for(CoreMap sentence: sentences) {
+            for(CoreLabel token: sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+                String word = token.get(CoreAnnotations.TextAnnotation.class);
+                System.out.println(word);
+            }
+            System.out.println("======================");
+        }
 
 
     }
